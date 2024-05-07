4c5f9e38775e731a9d682762118b7be2766aabc9
Aldinei <aldinei.franco@unimedprudente.com.br>
2024-05-07 08:36:18 -0300

    button, input, state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import { useState } from "react"
-import { Button } from "./components/Button"
+import { useEffect, useState } from "react"
+import { Button, ButtonI } from "./components/Button"
 import { Input } from "./components/Input"
+import './index.css'
+
+
+type User = {
+  nome: string
+  profissão: string
+}
+
 
 function App() {
-  const [adicioinar, setAdicionar] = useState(0)
+  const [adicioinar, setAdicionar] = useState(0);
+  const [data, setData] = useState<null | User>(null)
+  const [ total , setTotal ] = useState(0)
+
+  function user() {
+    return {
+      nome: "Aldinei",
+      profissão: "Dev"
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData(user())
+    }, 1000)
+  }, [])
+
 
   function handleAdd() {
     setAdicionar((total) => total + 1)
@@ -12,7 +36,14 @@ function App() {
   return (
     <div>
       <div>
-        <h2>Button</h2>
+        Total : {total}
+        <ButtonI incrementar={setTotal}/>
+      </div>
+      <div>
+        {data !== null && <div>{data.nome} - {data.profissão}</div>}
+      </div>
+      <div>
+        <h2 className="text-red-600">Button</h2>
         <p>{adicioinar}</p>
         <Button onClick={handleAdd} >Incrementar</Button>
       </div>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,15 @@ type ButtonProps = ComponentProps<'button'> & {
   tamanho?: string;
 }
 
+type ButtonPropsI = {
+  incrementar: React.Dispatch<React.SetStateAction<number>>
+}
+
+
+
 export function Button({ tamanho, children, ...rest }: ButtonProps) {
+  
+
   return (
     <button
       style={{ fontSize: tamanho }}
@@ -15,3 +23,10 @@ export function Button({ tamanho, children, ...rest }: ButtonProps) {
     </button>
   )
 }
+
+export function ButtonI({ incrementar }: ButtonPropsI ) {
+  return (
+    <button onClick={() => incrementar((n) => n + 1)}>ADD</button>
+  )
+}
+
